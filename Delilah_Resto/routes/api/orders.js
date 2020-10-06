d20d121b7341932704f5f5bf657f4468d1493b1d
JuanCarlosRamirez <58491643+JuanCarlosRamirez-dev@users.noreply.github.com>
2020-10-06 15:40:53 -0500

    create order on work

--- Delilah_Resto/routes/api/orders.js
+++ Delilah_Resto/routes/api/orders.js
@@ -7,5 +7,19 @@ module.exports = {
     getOrders: async (req, res) => {
         const order = await Order.findAll();
         res.send(order);
+    },
+
+    createOrder: async (req, res) => {
+        try {
+
+            const userToken = req.headers["user-token"]
+            let payload = {}
+            payload = jwt.decode(userToken, "secreto")
+            const userId = payload.usuarioId
+
+            const newOrder = await sequelize.query(`INSERT INTO orders ( id_user) VALUES ( ${userId})`)
+            res.send(newOrder)
+        }
+        catch (error) { res.send("Error: " + error) }
     }
 }

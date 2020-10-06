2623d63407431069dd514fdc10a2a8a157ff1bcb
JuanCarlosRamirez <58491643+JuanCarlosRamirez-dev@users.noreply.github.com>
2020-10-06 15:39:27 -0500

    API working good

--- Delilah_Resto/routes/api/orders.js
+++ Delilah_Resto/routes/api/orders.js
@@ -13,19 +13,7 @@ module.exports = {
     getOrders: async (req, res) => {
         const order = await Order.findAll();
         res.send(order);
-    },
-
-    createOrder: async (req, res) => {
-        try {
-
-            const userToken = req.headers["user-token"]
-            let payload = {}
-            payload = jwt.decode(userToken, "secreto")
-            const userId = payload.usuarioId
-
-            const newOrder = await sequelize.query(`INSERT INTO orders ( id_user) VALUES ( ${userId})`)
-            res.send(newOrder)
-        }
-        catch (error) { res.send("Error: " + error) }
     }
+
+
 }
